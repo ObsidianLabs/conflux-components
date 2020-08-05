d9e959ebceca87083f7461d837492c7b585caf83
Phil Li <qft.gtr@gmail.com>
2020-08-05 17:29:28 +0800

    fix: windows create instance

--- packages/conflux-instances/main/InstanceManager.js
+++ packages/conflux-instances/main/InstanceManager.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const os = require('os')
 const semverLt = require('semver/functions/lt')
 const semverValid = require('semver/functions/valid')
 
@@ -16,11 +17,11 @@ class InstanceManager extends IpcChannel {
   }
 
   async create ({ name, version, genesis_secrets, chain = 'dev' }) {
-    await this.exec(`docker volume create --label version=${version},chain=${chain} conflux-${name}`)
+    await this.cp(`docker volume create --label version=${version},chain=${chain} conflux-${name}`)
 
-    const configPath = path.join('/tmp', `${chain}.toml`)
-    const logPath = path.join('/tmp', `log.yaml`)
-    const genesis = path.join('/tmp', 'genesis_secrets.txt')
+    const configPath = path.join(os.tmpdir(), `${chain}.toml`)
+    const logPath = path.join(os.tmpdir(), `log.yaml`)
+    const genesis = path.join(os.tmpdir(), 'genesis_secrets.txt')
 
     const configContent = fs.readFileSync(path.join(__dirname, 'chain-configs', `${chain}.toml`))
     fs.writeFileSync(configPath, configContent)
@@ -30,17 +31,17 @@ class InstanceManager extends IpcChannel {
 
     fs.writeFileSync(genesis, genesis_secrets)
 
-    await this.exec(`docker run -d --rm -it --name conflux-config-${name} -v conflux-${name}:/conflux-node confluxchain/conflux-rust:${version} /bin/bash`)
-    await this.exec(`docker cp ${configPath} conflux-config-${name}:/conflux-node/default.toml`)
-    await this.exec(`docker cp ${logPath} conflux-config-${name}:/conflux-node/log.yaml`)
-    await this.exec(`docker cp ${genesis} conflux-config-${name}:/conflux-node/genesis_secrets.txt`)
-    await this.exec(`docker stop conflux-config-${name}`)
+    await this.cp(`docker run -d --rm -it --name conflux-config-${name} -v conflux-${name}:/conflux-node confluxchain/conflux-rust:${version} /bin/bash`)
+    await this.cp(`docker cp ${configPath} conflux-config-${name}:/conflux-node/default.toml`)
+    await this.cp(`docker cp ${logPath} conflux-config-${name}:/conflux-node/log.yaml`)
+    await this.cp(`docker cp ${genesis} conflux-config-${name}:/conflux-node/genesis_secrets.txt`)
+    await this.cp(`docker stop conflux-config-${name}`)
 
     fs.unlinkSync(genesis)
   }
 
   async list (chain = 'dev') {
-    const { logs: volumes } = await this.exec(`docker volume ls --format "{{json . }}"`)
+    const { logs: volumes } = await this.cp(`docker volume ls --format "{{json . }}"`)
     const instances = volumes.split('\n').filter(Boolean).map(JSON.parse).filter(x => x.Name.startsWith('conflux-'))
     const instancesWithLabels = instances.map(i => {
       const labels = {}
@@ -55,7 +56,7 @@ class InstanceManager extends IpcChannel {
   }
 
   async delete (name) {
-    await this.exec(`docker volume rm conflux-${name}`)
+    await this.cp(`docker volume rm conflux-${name}`)
   }
 }
 
