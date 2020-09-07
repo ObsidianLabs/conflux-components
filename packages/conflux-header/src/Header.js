1d54e1f98314ebd4b4029c64b5a930f0b62933cd
Phil <qft.gtr@gmail.com>
2020-09-07 22:17:16 +0800

    show keypair manger keys in header

--- packages/conflux-header/src/Header.js
+++ packages/conflux-header/src/Header.js
@@ -1,11 +1,26 @@
 import React, { PureComponent } from 'react'
 
 import Navbar from '@obsidians/navbar'
+import keypairManager from '@obsidians/keypair'
 import { NewProjectModal, navbarItem } from '@obsidians/conflux-project' 
 
 import headerActions from './headerActions'
 
 export default class Header extends PureComponent {
+  constructor (props) {
+    super(props)
+    this.state = {
+      keypairs: []
+    }
+  }
+
+  componentDidMount () {
+    keypairManager.loadAllKeypairs().then(this.updateKeypairs)
+    keypairManager.onUpdated(this.updateKeypairs)
+  }
+
+  updateKeypairs = keypairs => this.setState({ keypairs })
+
   render () {
     const {
       projects,
@@ -22,16 +37,29 @@ export default class Header extends PureComponent {
       navbarItem(projects, selectedProject)
     ]
 
-    const dropdownItems = starred.length
-      ? starred.map(item => ({ id: item, name: <code>{item}</code> }))
-      : [{ none: true }]
+    const dropdownKeypairs = this.state.keypairs.map(k => ({ id: k.address, name: k.name || <code>{k.address.substr(0, 6)}...{k.address.substr(-4)}</code> }))
+    dropdownKeypairs.unshift({ header: 'keypair manager' })
+    if (!dropdownKeypairs.length) {
+      dropdownKeypairs.push({ none: true })
+    }
+    const dropdownStarred = starred.map(item => ({ id: item, name: <code>{item.substr(0, 6)}...{item.substr(-4)}</code> }))
+    const dropdownStarredInContract = [{ header: 'starred' }, ...dropdownStarred]
+    if (dropdownStarred.length) {
+      dropdownStarred.unshift({ header: 'starred' })
+      dropdownStarred.unshift({ divider: true })
+    } else {
+      dropdownStarredInContract.push({ none: true })
+    }
+
+    const accountName = selectedAccount && (this.state.keypairs.find(k => k.address === selectedAccount)?.name || <code>{selectedAccount}</code>)
+
     const navbarRight = [
       {
         route: 'contract',
         title: 'Contract',
         icon: 'fa-file-invoice',
         selected: { id: selectedContract, name: selectedContract && <code>{selectedContract}</code> },
-        dropdown: [{ header: 'starred' }, ...dropdownItems],
+        dropdown: dropdownStarredInContract,
         onClickItem: selected => headerActions.selectContract(network.id, selected),
         contextMenu: () => [{
           text: 'Remove from Starred',
@@ -42,8 +70,8 @@ export default class Header extends PureComponent {
         route: 'account',
         title: 'Explorer',
         icon: 'fa-file-invoice',
-        selected: { id: selectedAccount, name: selectedAccount && <code>{selectedAccount}</code> },
-        dropdown: [{ header: 'starred' }, ...dropdownItems],
+        selected: { id: selectedAccount, name: accountName },
+        dropdown: [...dropdownKeypairs, ...dropdownStarred],
         onClickItem: selected => headerActions.selectAccount(network.id, selected),
         contextMenu: () => [{
           text: 'Remove from Starred',
