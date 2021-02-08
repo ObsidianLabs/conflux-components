b619078166514e4b8a346b2481154a858b72d15f
Phil <qft.gtr@gmail.com>
2021-02-08 13:58:36 +0800

    Update index.js

--- packages/conflux-sdk/src/index.js
+++ packages/conflux-sdk/src/index.js
@@ -1,6 +1,5 @@
 export { default } from './Sdk'
 
-export { abi } from 'js-conflux-sdk'
 export { default as txOptions } from './txOptions'
 export { default as utils } from './utils'
 export { default as kp } from './kp'
