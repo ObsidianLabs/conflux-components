9c3186c2177fd43e4ab2c199da8bbe30ed0a0256
Phil <qft.gtr@gmail.com>
2021-02-09 13:35:28 +0800

    utils.display

--- packages/conflux-sdk/src/utils.js
+++ packages/conflux-sdk/src/utils.js
@@ -1,5 +1,17 @@
 import { util, abi, format, sign, Drip } from 'js-conflux-sdk'
 
+const display = value => {
+  const amount = Drip(value).toCFX()
+  if (amount > 0.001) {
+    return `${new Intl.NumberFormat().format(amount)} CFX`
+  } else if (amount > 0.0000000001) {
+    const gvalue = Drip(value).toGDrip()
+    return `${new Intl.NumberFormat().format(gvalue)} Gdrip`
+  } else {
+    return `${new Intl.NumberFormat().format(value)} drip`
+  }
+}
+
 export default {
   sign: {
     sha3: str => `0x${sign.sha3(Buffer.from(str.replace('0x', ''), 'hex')).toString('hex')}`
@@ -14,5 +26,6 @@ export default {
     toValue: Drip.fromCFX,
     valueToGvalue: value => Drip(value).toGDrip()
   },
+  display,
   decodeError: data => abi.errorCoder.decodeError({ data }).message
 }
