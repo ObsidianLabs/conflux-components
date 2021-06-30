031f2c85ca24a601b7a1b4dd96a524d8c4c049eb
Phil Li <qft.gtr@gmail.com>
2021-06-30 00:13:28 -0700

    Update index.js

--- packages/conflux-sdk/src/redux/index.js
+++ packages/conflux-sdk/src/redux/index.js
@@ -17,6 +17,11 @@ export default {
         return state.set(payload.codeHash, Map(payload))
       }
     },
+    ABI_UPDATE: {
+      reducer: (state, { payload }) => {
+        return state.update(payload.codeHash, () => Map(payload))
+      }
+    },
     ABI_DELETE: {
       reducer: (state, { payload }) => state.remove(payload)
     },
